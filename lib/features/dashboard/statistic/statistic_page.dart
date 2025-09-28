e7586b1cc05fe88cf033467c4fd94164486ce5e1
tungdh12 <tungdh12@viettel.com.vn>
2025-09-28 18:05:18 +0700

    refresh page

--- lib/features/dashboard/statistic/statistic_page.dart
+++ lib/features/dashboard/statistic/statistic_page.dart
@@ -28,13 +28,17 @@ class _StatisticPageState extends State<StatisticPage> {
     super.initState();
     mongo = MongoService();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      _statusBook = await mongo.getBorrowStats();
-      _top5HighestBorrowedBook = await mongo.getTop5Categories();
-      _borrowedBookByMonth = await mongo.getBorrowStatsLast5Months();
-      _libraryStats = await mongo.getLibraryStats();
-
-      setState(() {});
+      _loadStatisticData();
     });
+  }
+
+  Future<void> _loadStatisticData() async {
+    _statusBook = await mongo.getBorrowStats();
+    _top5HighestBorrowedBook = await mongo.getTop5Categories();
+    _borrowedBookByMonth = await mongo.getBorrowStatsLast5Months();
+    _libraryStats = await mongo.getLibraryStats();
+
+    setState(() {});
   }
 
   @override
@@ -48,127 +52,132 @@ class _StatisticPageState extends State<StatisticPage> {
 
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Center(
-                child: Text(
-                  'Library Book Statistics',
-                  style: TextStyle(fontSize: 24, color: AppColors.bgColor),
-                ),
-              ),
-
-              if (_libraryStats != null) ...[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    children: [
-                      Gap.h40,
-                      Column(
-                        children: [
-                          _buildStatsItem(
-                            title: 'Total Books:',
-                            content: (_libraryStats?.totalBooks ?? 0)
-                                .toString(),
-                          ),
-                          _buildStatsItem(
-                            title: 'Total Borrowed Books:',
-                            content: (_libraryStats?.borrowedBooks ?? 0)
-                                .toString(),
-                          ),
-                          _buildStatsItem(
-                            title: 'Total Authors:',
-                            content: (_libraryStats?.totalAuthors ?? 0)
-                                .toString(),
-                          ),
-                          _buildStatsItem(
-                            title: 'Total Categories:',
-                            content: (_libraryStats?.totalCategories ?? 0)
-                                .toString(),
-                          ),
-                          _buildStatsItem(
-                            title: 'Total Users:',
-                            content: (_libraryStats?.totalReaders ?? 0)
-                                .toString(),
-                          ),
-                        ],
-                      ),
-                    ],
+        child: RefreshIndicator(
+          onRefresh: () async {
+            _loadStatisticData();
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Center(
+                  child: Text(
+                    'Library Book Statistics',
+                    style: TextStyle(fontSize: 24, color: AppColors.bgColor),
                   ),
                 ),
-              ],
 
-              if (_statusBook != null) ...[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    children: [
-                      Gap.h40,
-                      CommonPieChart(
-                        title: 'Library Book Status Statistics',
-                        items: [
-                          PieChartItem(
-                            label: "Borrowed",
-                            value: (_statusBook!['borrowed']).toDouble(),
-                            color: Colors.orangeAccent,
-                          ),
-                          PieChartItem(
-                            label: "Available",
-                            value: (_statusBook!['available']).toDouble(),
-                            color: Colors.teal,
-                          ),
-                          PieChartItem(
-                            label: "Overdue",
-                            value: (_statusBook!['overdue']).toDouble(),
-                            color: Colors.redAccent,
-                          ),
-                        ],
-                      ),
-                    ],
+                if (_libraryStats != null) ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      children: [
+                        Gap.h40,
+                        Column(
+                          children: [
+                            _buildStatsItem(
+                              title: 'Total Books:',
+                              content: (_libraryStats?.totalBooks ?? 0)
+                                  .toString(),
+                            ),
+                            _buildStatsItem(
+                              title: 'Total Borrowed Books:',
+                              content: (_libraryStats?.borrowedBooks ?? 0)
+                                  .toString(),
+                            ),
+                            _buildStatsItem(
+                              title: 'Total Authors:',
+                              content: (_libraryStats?.totalAuthors ?? 0)
+                                  .toString(),
+                            ),
+                            _buildStatsItem(
+                              title: 'Total Categories:',
+                              content: (_libraryStats?.totalCategories ?? 0)
+                                  .toString(),
+                            ),
+                            _buildStatsItem(
+                              title: 'Total Users:',
+                              content: (_libraryStats?.totalReaders ?? 0)
+                                  .toString(),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
 
-              if (_top5HighestBorrowedBook.isNotEmpty) ...[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    children: [
-                      Gap.h40,
-                      CommonBarChart(
-                        title: "Top 5 Borrowed Book Genres",
-                        data: dataBarChart,
-                        labels: labelBarChart,
-                        barColor: AppColors.mainColorYellow,
-                      ),
-                    ],
+                if (_statusBook != null) ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      children: [
+                        Gap.h40,
+                        CommonPieChart(
+                          title: 'Library Book Status Statistics',
+                          items: [
+                            PieChartItem(
+                              label: "Borrowed",
+                              value: (_statusBook!['borrowed']).toDouble(),
+                              color: Colors.orangeAccent,
+                            ),
+                            PieChartItem(
+                              label: "Available",
+                              value: (_statusBook!['available']).toDouble(),
+                              color: Colors.teal,
+                            ),
+                            PieChartItem(
+                              label: "Overdue",
+                              value: (_statusBook!['overdue']).toDouble(),
+                              color: Colors.redAccent,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
 
-              if (_borrowedBookByMonth.isNotEmpty) ...[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    children: [
-                      Gap.h40,
-                      CommonLineChart(
-                        title: "Borrowed Books Over 5 Months",
-                        data: _borrowedBookByMonth
-                            .map(
-                              (e) => ChartDataPoint(
-                                label: e.month ?? '',
-                                value: (e.borrowCount ?? 0).toDouble(),
-                              ),
-                            )
-                            .toList(),
-                        lineColor: Colors.green,
-                      ),
-                    ],
+                if (_top5HighestBorrowedBook.isNotEmpty) ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      children: [
+                        Gap.h40,
+                        CommonBarChart(
+                          title: "Top 5 Borrowed Book Genres",
+                          data: dataBarChart,
+                          labels: labelBarChart,
+                          barColor: AppColors.mainColorYellow,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                ],
+
+                if (_borrowedBookByMonth.isNotEmpty) ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      children: [
+                        Gap.h40,
+                        CommonLineChart(
+                          title: "Borrowed Books Over 5 Months",
+                          data: _borrowedBookByMonth
+                              .map(
+                                (e) => ChartDataPoint(
+                                  label: e.month ?? '',
+                                  value: (e.borrowCount ?? 0).toDouble(),
+                                ),
+                              )
+                              .toList(),
+                          lineColor: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ],
-            ],
+            ),
           ),
         ),
       ),
